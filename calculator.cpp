226d23ba7feb284a395a01eb1f28893b3e0e2ace
Sergii Piatakov <sergii.piatakov@globallogic.com>
2018-11-15 15:43:25 +0200

    add operation logs in stdout

--- calculator.cpp
+++ calculator.cpp
@@ -1,11 +1,19 @@
 #include "calculator.h"
 
 int Calculator::Add (double a, double b)
-{
+{   
+    std::cout << "Adding " << a << " and " << b << std::endl;
 	return a + b + 0.5;
 }
 
 int Calculator::Sub (double a, double b)
-{
+{   
+    std::cout << "Subtracting " << b << " from " << a << std::endl;
     return Add (a, -b);
+}
+
+int Calculator::Mul (double a, double b)
+{
+    std::cout << "Multiplying " << a << " by " << b << std::endl;
+    return a * b + 0.5;
 }
